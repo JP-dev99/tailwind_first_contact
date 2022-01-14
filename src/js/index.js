836114f498a1578939b6e79f197cef862165720c
João Pedro G <joaocitimo@gmail.com>
2022-01-14 20:18:21 -0300

    set_dark_mode

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
-const test = '<p class="text-red-800">teste</p>';
+const checkbox = document.querySelector("#checkbox");
+const html = document.querySelector("html");
+
+const toggleDarkMode = function () {
+  checkbox.checked
+  ? html.classList.add("dark")
+  : html.classList.remove("dark");
+}
+
+//calling the function directly
+
+toggleDarkMode();
+checkbox.addEventListener("click",toggleDarkMode);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["*.{html,js}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+        100: '#c542f5',
+        200: '#9e35c4',
+        300: '#6d2487',
+        400: '#331040',
+      }
+    }
   },
+},
+variants: {
+  extend: {},
+},
   plugins: [],
 }
